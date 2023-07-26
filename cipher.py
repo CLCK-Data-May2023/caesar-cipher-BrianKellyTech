c096dc034f4e714cecd9ed757c26719a89c5dc58
Brian Kelly <brian.kelly.tech@gmail.com>
2023-07-25 20:30:06 -0400

    Cypher amended to only deal with lowercase letters.

--- cipher.py
+++ cipher.py
@@ -5,19 +5,14 @@ cipher = {
     "g" : "l", "h" : "m", "i" : "n", "j" : "o", "k" : "p", "l" : "q",
     "m" : "r", "n" : "s", "o" : "t", "p" : "u", "q" : "v", "r" : "w",
     "s" : "x", "t" : "y", "u" : "z", "v" : "a", "w" : "b", "x" : "c",
-    "y" : "d", "z" : "e",
-
-    "A" : "F", "B" : "G", "C" : "H", "D" : "I", "E" : "J", "F" : "K",
-    "G" : "L", "H" : "M", "I" : "N", "J" : "O", "K" : "P", "L" : "Q",
-    "M" : "R", "N" : "S", "O" : "T", "P" : "U", "Q" : "V", "R" : "W",
-    "S" : "X", "T" : "Y", "U" : "Z", "V" : "A", "W" : "B", "X" : "C",
-    "Y" : "D", "Z" : "E"
+    "y" : "d", "z" : "e"
 }
 
-plain_text = input("Please enter a sentence to be encrypted:")
+plaintext = input("Please enter a sentence to be encrypted:")
+plaintext = plaintext.lower()
 
 result = ''
-for letter in plain_text:
+for letter in plaintext:
     if letter in cipher:
         result = result + cipher[letter]
     else:
